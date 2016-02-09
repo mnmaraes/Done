b2fa03ebe77a2045aeb9b85d64dd8f7b179d5f95
Murillo Nicacio de Maraes <mnmaraes@gmail.com>
2016-02-09 18:16:29 -0300

    Adds M/V/VM projects

---
+++ View/View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by Murillo Nicacio de Maraes on 2/9/16.
+//  Copyright © 2016 Unreasonable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
---
+++ ViewModel/ViewModel/ViewModel.h
@@ -0,0 +1,19 @@
+//
+//  ViewModel.h
+//  ViewModel
+//
+//  Created by Murillo Nicacio de Maraes on 2/9/16.
+//  Copyright © 2016 Unreasonable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewModel.
+FOUNDATION_EXPORT double ViewModelVersionNumber;
+
+//! Project version string for ViewModel.
+FOUNDATION_EXPORT const unsigned char ViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModel/PublicHeader.h>
+
+
